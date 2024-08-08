90bb325f9e3dd71083965263fd0ebcd2c1bf6386
Jofred <reinosaj@gmail.com>
2024-08-08 16:50:58 -0400

    copy with angular cdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private clipboard = inject(Clipboard)
   title = 'angularCdk';
+  value = '';
+
+  copyText() {
+    if (!this.value.trim()) {
+      alert('Nothing to copy');
+      return;
+    }
+
+    this.clipboard.copy(this.value);
+    alert('Copied');
+
+  }
 }
